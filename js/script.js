cce73d1fd98c51a929a15f52976610f1732f95a5
corner0303 <corner0303@gmail.com>
2020-12-31 18:00:28 +0900

    workの完成

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
-$(function() {
-  $('.hamburger_btn').click(function() {
-      $(this).toggleClass('--active');
-      $('.gnav').slideToggle();
-      
-      if($('.hamburger_btn__text').text() == 'Close'){
-        $('.hamburger_btn__text').text('Menu')
-      }else{
-        $('.hamburger_btn__text').text('Close')
-      }
+$(function () {
+  $('.hamburger_btn').click(function () {
+    $(this).toggleClass('--active');
+    $('.gnav').slideToggle();
+
+    if ($('.hamburger_btn__text').text() == 'Close') {
+      $('.hamburger_btn__text').text('Menu')
+    } else {
+      $('.hamburger_btn__text').text('Close')
+    }
   });
 });
 
@@ -18,3 +18,39 @@ $(function () {
   $("body").css("margin-top", height);//10pxだけ余裕をもたせる
 });
 
+
+$('.slider').slick({
+  dots: true,
+  slidesToShow: 3,
+  initialSlide: 2,
+  centerMode: true,
+  autoplay: true,
+  waitForAnimate: true,
+  prevArrow: '<div class="prev"><i class="slider_arrow_icon prev_arrow fas fa-arrow-left"></i></div>',
+  nextArrow: '<div class="next"><i class="slider_arrow_icon next_arrow fas fa-arrow-right"></i></div>',
+
+  responsive: [
+    {
+      breakpoint: 1024, // 768~1023px以下のサイズに適用
+      settings: {
+        slidesToShow: 3,
+        centerPadding: '50px',
+
+      }
+    }, {
+      breakpoint: 768, // 480〜767px以下のサイズに適用
+      settings: {
+        slidesToShow: 2,
+        centerPadding: '15px',
+
+      }
+    }, {
+      breakpoint: 480, // 〜479px以下のサイズに適用
+      settings: {
+        slidesToShow: 1,
+        centerPadding: '15px',
+        
+      }
+    }
+  ]
+});
